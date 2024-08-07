e570a1d44a00a2194c60bc7e1d9eaec7645aa01c
Ichchha Dhungel <ichchhadhungel@gmail.com>
2024-08-07 15:51:07 +0545

    API

--- routes/api.php
+++ routes/api.php
@@ -16,7 +16,8 @@ Route::group(['middleware' => 'api'], function()
     Route::post('/login', 'LoginController@login');
 });
 
-Route::middleware(['auth:api'])->prefix('v1')->group(function () {
+Route::middleware(['auth:api'])->prefix('v1')->group(function ()
+{
     Route::get('/getPosts', 'PostsController@getPosts');
     Route::post('/createPosts', 'PostsController@createPosts');
     Route::put('/updatePosts/{id}', 'PostsController@updatePosts');
@@ -27,16 +28,3 @@ Route::middleware(['auth:api'])->prefix('v1')->group(function () {
     Route::put('/updateComments/{id}/{post_id}', 'PostsController@updateComments');
     Route::post('/deleteComments', 'PostsController@deleteComments');
 });
-/*
-Route::group(['middleware' => ['auth:api']], function ()
-{
-    Route::get('/getPosts', [PostsController::class, 'getPosts']);
-    Route::post('/createPosts', [PostsController::class, 'createPosts']);
-    Route::put('/updatePosts/{id}', [PostsController::class, 'updatePosts']);
-    Route::post('/deletePosts', [PostsController::class, 'deletePosts']);
-
-    Route::get('/getComments/{post_id}', [PostsController::class, 'getComments']);
-    Route::post('/createComments', [PostsController::class, 'createComments']);
-    Route::put('/updateComments/{id}/{post_id}', [PostsController::class, 'updateComments']);
-    Route::post('/deleteComments', [PostsController::class, 'deleteComments']);
-});*/
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,30 @@
+
 <?php
 
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+use App\Http\Controllers\PostsController;
+use App\Http\Controllers\CommentsController;
+use App\Http\Controllers\LoginController;
+use App\Http\Controllers\UserController;
+
+Route::post('/register', 'UserController@register');
+
+Route::group(['middleware' => 'api'], function()
+{
+    Route::post('/login', 'LoginController@login');
+});
+
+Route::middleware(['auth:api'])->prefix('v1')->group(function ()
+{
+    Route::get('/getPosts', 'PostsController@getPosts');
+    Route::post('/createPosts', 'PostsController@createPosts');
+    Route::put('/updatePosts/{id}', 'PostsController@updatePosts');
+    Route::post('/deletePosts', 'PostsController@deletePosts');
+
+    Route::get('/getComments/{post_id}', 'PostsController@getComments');
+    Route::post('/createComments', 'PostsController@createComments');
+    Route::put('/updateComments/{id}/{post_id}', 'PostsController@updateComments');
+    Route::post('/deleteComments', 'PostsController@deleteComments');
 });
